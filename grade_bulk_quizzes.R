cd807092b5480e219cd6857811d573c5541250ce
conormcmahon <conormcmahon22@gmail.com>
2024-07-02 14:42:50 -0700

    Added ability to set 'deadline' and not count quizzes submitted late.

--- grade_bulk_quizzes.R
+++ grade_bulk_quizzes.R
@@ -2,12 +2,23 @@
 library(tidyverse)
 library(janitor)
 
+# ------------------------------ User Parameters ------------------------------
+# ***** NOTE *****
+# Please add options for the strings below to make this work for your use file system
+
+# Specify the location where coarser quiz summaries are stored
+quiz_directory <- ""
+# Specify the location where detailed quiz data are stored
+question_directory <- ""
+# Set due date for quizzes
+deadline <- as.Date("2024/06/28")
+# Set filepath where output grade CSV will be written
+output_filepath <- ""
+
 # ------------------------------ Quiz-level Summaries ------------------------------
 # These files have a single dataframe for each quiz which includes coarse information
 # This does not distinguish between students who answered a question wrong and those who did not attempt it
 
-# Specify the location where coarser quiz summaries are stored
-quiz_directory <- ""
 # Get list of files within target directory
 quiz_files <- list.files(quiz_directory, full.names = TRUE)
 # Retain only the files which end in '.csv'
@@ -29,8 +40,6 @@ all_quiz_results <- bind_rows(all_quiz_results) %>%
 # These files have a single dataframe for each question, with multiple per quiz
 # Notably, this tracks which student attempted which question, so it allows computing completion grades
 
-# Specify the location where detailed quiz data are stored
-question_directory <- ""
 # Get list of files within target directory
 question_files <- list.files(question_directory, full.names = TRUE)
 # Retain only the files which end in '.csv'
@@ -44,9 +53,25 @@ all_question_results <- lapply(question_files,
                                  new_data$filepath <- filepath
                                  new_data$filename <- basename(filepath) 
                                  new_data$filename <- substr(new_data$filename, 1, nchar(new_data$filename)-4)
-                                 print(new_data$filename)
                                  new_data$quiz_index <- as.numeric(str_split((new_data$filename)[[1]], "_")[[1]][[2]])
                                  new_data$question_index <- as.numeric(str_split((new_data$filename)[[1]], "_")[[1]][[4]])
+                                 date_string_list <- str_split(substr(new_data$'Quiz Start Time', 1, 9), "/")
+                                 new_data$month <- unlist(lapply(date_string_list,
+                                                                 function(date_str_list){
+                                                                   return(as.numeric(date_str_list[[1]]))
+                                                                 }))
+                                 new_data$day <- unlist(lapply(date_string_list,
+                                                                 function(date_str_list){
+                                                                   return(as.numeric(date_str_list[[2]]))
+                                                                 }))
+                                 new_data$year <- unlist(lapply(date_string_list,
+                                                                 function(date_str_list){
+                                                                   return(as.numeric(date_str_list[[3]]))
+                                                                 }))
+                                 new_data$date <- lubridate::ymd(paste(new_data$year,
+                                                                       new_data$month,
+                                                                       new_data$day,
+                                                                       sep="/"))
                                  return(new_data)
                                })
 # Reformat to a single data frame with all the quiz data
@@ -61,12 +86,20 @@ gradebook_summary <- all_question_results %>%
   group_by(user_display_name, 
            quiz_index) %>% 
   summarize(total_attempts = n(), 
-            correct_attempts = sum(correct)) %>% 
+            correct_attempts = sum(correct),
+            on_time_attempts = sum(date <= deadline),
+            on_time_correct_attempts = sum(correct*(date <= deadline))) %>% 
   group_by(quiz_index) %>% 
-  mutate(fraction_correct = correct_attempts / total_attempts, 
-         fraction_attempted = total_attempts/max(total_attempts)) 
+  mutate(fraction_correct = on_time_correct_attempts / total_attempts, 
+         fraction_attempted = on_time_attempts/max(total_attempts)) 
 gradebook_summary %>% View()
+# Add surname field
+names <- gradebook_summary$user_display_name
+surnames <- str_split(names, " ")
+surnames <- unlist(lapply(surnames,
+                          function(str_list){ return(str_list[[2]]) }))
+gradebook_summary$surname <- surnames
 
 # Write Output
 
-write_csv(gradebook_summary, "")
+write_csv(gradebook_summary %>% arrange(quiz_index, surnames), output_filepath, append = FALSE)
